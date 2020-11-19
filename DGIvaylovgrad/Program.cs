2640c1654ece1fb58155d642d3fdb94bcb4b57e3
veselin9931 <ves.georgiev99@gmail.com>
2020-11-19 22:07:02 +0200

    asd

--- DGIvaylovgrad/Program.cs
+++ DGIvaylovgrad/Program.cs
@@ -14,7 +14,7 @@ namespace DGIvaylovgrad
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
+        }  
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
