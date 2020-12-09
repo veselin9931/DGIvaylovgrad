935e359fee254eca3bd3820502573e995b5a93a6
veselin9931 <ves.georgiev99@gmail.com>
2020-12-09 16:46:52 +0200

    details

--- DGIvaylovgrad/Program.cs
+++ DGIvaylovgrad/Program.cs
@@ -19,13 +19,6 @@ namespace DGIvaylovgrad
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("ApiSecret"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
